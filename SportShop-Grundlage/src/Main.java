e46f9c06ffce96393c351507528c35dab7b41923
Kateryna Kocherova <kocherova18@gmail.com>
2026-06-10 23:29:11 +0200

    Update Main.java

--- SportShop-Grundlage/src/Main.java
+++ SportShop-Grundlage/src/Main.java
@@ -1,14 +1,6 @@
-import data.DataManager;
-import model.Product;
-
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        DataManager dataManager = new DataManager();
 
-        List<Product> products = dataManager.loadProducts();
-        System.out.println("SportShop gestartet.");
-        System.out.println("Geladene Produkte: " + products.size());
+    public static void main(String[] args) {
+
     }
 }
