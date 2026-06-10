de7a81f21072cdbb0de7571f7a486350ca4e9e97
Kateryna Kocherova <kocherova18@gmail.com>
2026-06-10 23:13:44 +0200

    Refactor OrderService with TODO for implementation
    Removed implementation details and added a TODO for future implementation.

--- SportShop-Grundlage/src/service/OrderService.java
+++ SportShop-Grundlage/src/service/OrderService.java
@@ -1,39 +1,7 @@
 package service;
 
-import model.CartItem;
-import model.Order;
-import model.User;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 public class OrderService {
-    private List<Order> orders;
-
-    public OrderService(List<Order> orders) {
-        this.orders = orders != null ? orders : new ArrayList<>();
-    }
-
-    public Order createOrder(User customer, List<CartItem> cartItems) {
-        // TODO Person 3: Bestellung aus Warenkorb erzeugen und speichern
-        int newOrderId = orders.size() + 1;
-        Order order = new Order(newOrderId, customer, cartItems, Order.STATUS_CREATED, LocalDateTime.now());
-        orders.add(order);
-        return order;
-    }
-
-    public List<Order> getOrdersByUser(User customer) {
-        // TODO Person 3: nur Bestellungen dieses Kunden zurueckgeben
-        return new ArrayList<>();
-    }
-
-    public List<Order> getAllOrders() {
-        return orders;
-    }
-
-    public boolean updateOrderStatus(int orderId, String status) {
-        // TODO Person 4/Admin: Status ERSTELLT, BEZAHLT oder VERSENDET setzen
-        return false;
+    // TODO: Implementierung durch Person 3
+}
     }
 }
