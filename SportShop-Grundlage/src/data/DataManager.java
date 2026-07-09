ab64daa6fdf7297662e59dbe84290ccb83568ec6
Kateryna Kocherova <kocherova18@gmail.com>
2026-07-09 19:47:00 +0200

    Implement DataManager serialization

--- SportShop-Grundlage/src/data/DataManager.java
+++ SportShop-Grundlage/src/data/DataManager.java
@@ -1,5 +1,109 @@
 package data;
 
+import model.User;
+import model.Product;
+import model.Order;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataManager {
-    // TODO: Serialisierung durch Person 4
+
+    private static final String USERS_FILE = "data/users.dat";
+    private static final String PRODUCTS_FILE = "data/products.dat";
+    private static final String ORDERS_FILE = "data/orders.dat";
+
+    public void saveUsers(List<User> users) {
+        try {
+            FileOutputStream fs = new FileOutputStream(USERS_FILE);
+            ObjectOutputStream out = new ObjectOutputStream(fs);
+
+            out.writeObject(users);
+
+            out.close();
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    public List<User> loadUsers() {
+        try {
+            FileInputStream fs = new FileInputStream(USERS_FILE);
+            ObjectInputStream in = new ObjectInputStream(fs);
+
+            List<User> users = (List<User>) in.readObject();
+
+            in.close();
+            return users;
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.toString());
+        }
+
+        return new ArrayList<User>();
+    }
+
+    public void saveProducts(List<Product> products) {
+        try {
+            FileOutputStream fs = new FileOutputStream(PRODUCTS_FILE);
+            ObjectOutputStream out = new ObjectOutputStream(fs);
+
+            out.writeObject(products);
+
+            out.close();
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    public List<Product> loadProducts() {
+        try {
+            FileInputStream fs = new FileInputStream(PRODUCTS_FILE);
+            ObjectInputStream in = new ObjectInputStream(fs);
+
+            List<Product> products = (List<Product>) in.readObject();
+
+            in.close();
+            return products;
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.toString());
+        }
+
+        return new ArrayList<Product>();
+    }
+
+    public void saveOrders(List<Order> orders) {
+        try {
+            FileOutputStream fs = new FileOutputStream(ORDERS_FILE);
+            ObjectOutputStream out = new ObjectOutputStream(fs);
+
+            out.writeObject(orders);
+
+            out.close();
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    public List<Order> loadOrders() {
+        try {
+            FileInputStream fs = new FileInputStream(ORDERS_FILE);
+            ObjectInputStream in = new ObjectInputStream(fs);
+
+            List<Order> orders = (List<Order>) in.readObject();
+
+            in.close();
+            return orders;
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.toString());
+        }
+
+        return new ArrayList<Order>();
+    }
 }
