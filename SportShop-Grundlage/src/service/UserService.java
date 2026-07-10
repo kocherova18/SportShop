ac838e050e6102bcfa97436eb57b670634cce47b
zuzanna.joziak <Zuzanna.E.Joziak@accenture.com>
2026-07-10 16:41:09 +0200

    added password validation

--- SportShop-Grundlage/src/service/UserService.java
+++ SportShop-Grundlage/src/service/UserService.java
@@ -19,7 +19,6 @@ public class UserService {
             return false;       //dann wird er keine Fehlermeldung bekommen
         }
         String checkedEmail = email.trim();     //trim löscht Leerzeichen von Email-Adresse
-
         for(User user:users){
             if(user.getEmail().equalsIgnoreCase(checkedEmail)){     //identifiziert große und kleine Buchstaben als derselbe
                 return true;
@@ -37,6 +36,12 @@ public class UserService {
 
         if(!checkedEmail.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]\\.[A-Za-z]{2,}$")){ //^bedeutet Anfang
             throw new IllegalArgumentException("Email hat ein ungültiges Format");
+        }
+    }
+
+    private void validatePassword(String password){
+        if(password == null || password.length()<8){
+            throw new IllegalArgumentException("Passwort muss mindestens 8 Zeichen enthalten");
         }
     }
 
