800b213d5a714dfbf658b05d961f356a3fc03614
zuzanna.joziak <Zuzanna.E.Joziak@accenture.com>
2026-07-14 12:12:34 +0200

    added and tested login frame

--- SportShop-Grundlage/src/Main.java
+++ SportShop-Grundlage/src/Main.java
@@ -2,7 +2,7 @@ import data.DataManager;
 import model.User;
 import service.UserService;
 import ui.RegisterFrame;
-
+import ui.LoginFrame;
 import java.sql.SQLOutput;
 
 public class Main {
@@ -11,26 +11,7 @@ public class Main {
         DataManager dataManager = new DataManager();
         UserService userService = new UserService(dataManager);
 
-        //RegisterFrame registerFrame = new RegisterFrame(userService);
-        //registerFrame.setVisible(true);
-
-        try{
-            User user = userService.login("example@example.com", "password123");
-
-            System.out.println("Login erfolgreich.");
-            System.out.println("Eingeloggt als: " + user.getEmail());
-        }catch(IllegalArgumentException e){
-            System.out.println("Fehler: " + e.getMessage());
-        }
-
-       /* try{
-            User user = userService.register("example@example.com", "password123");
-
-            System.out.println("Registrierung erfolgreich.");
-            System.out.println("Benutzer: " + user);
-        } catch (IllegalArgumentException e){
-            System.out.println("Fehler: " + e.getMessage());
-        }
-        */
+        LoginFrame loginFrame = new LoginFrame(userService);
+        loginFrame.setVisible(true);
     }
 }
