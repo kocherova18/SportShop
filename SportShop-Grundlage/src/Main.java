5b5f3a08b2d4f9a6b33b62c653787327fef7539c
zuzanna.joziak <Zuzanna.E.Joziak@accenture.com>
2026-07-20 12:45:04 +0200

    deleted unnessesary background images

--- SportShop-Grundlage/src/Main.java
+++ SportShop-Grundlage/src/Main.java
@@ -8,8 +8,7 @@ public class Main {
     public static void main(String[] args) {
         DataManager dataManager = new DataManager();
 
-        AdminService adminService =
-                new AdminService(dataManager);
+        AdminService adminService = new AdminService(dataManager);
 
         adminService.createDefaultAdminIfMissing();
 
